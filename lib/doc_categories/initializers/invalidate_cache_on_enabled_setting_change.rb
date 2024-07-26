139cd71c04e9eb2e180078f3bc8d957723ddb500
Sérgio Saquetim <saquetim@discourse.org>
2024-07-26 14:48:29 -0300

    Fix rubocop linting issue

--- lib/doc_categories/initializers/invalidate_cache_on_enabled_setting_change.rb
+++ lib/doc_categories/initializers/invalidate_cache_on_enabled_setting_change.rb
@@ -6,6 +6,8 @@ module ::DocCategories
       def apply
         # DiscourseEvent is used below intentionally because we want this code to work when the plugin is disabled
         # `plugin.on`, wouldn't
+
+        # rubocop:disable Discourse/Plugins/UsePluginInstanceOn
         DiscourseEvent.on(:site_setting_changed) do |name|
           # invalidates the site cache if the plugin is turned on or off
           Site.clear_cache if name == plugin.enabled_site_setting
