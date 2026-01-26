1950718eb03c8b9426bee9f444a68fad6f2ed604
Jarek Radosz <jarek@cvx.dev>
2026-01-26 20:12:58 +0100

    DEV: Update linting and add types

--- spec/serializers/basic_category_serializer_spec.rb
+++ spec/serializers/basic_category_serializer_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe BasicCategorySerializer do
   fab!(:category, :category_with_definition)
   fab!(:documentation_category, :category_with_definition)
